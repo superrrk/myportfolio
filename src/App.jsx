7ab9140966024e1e04c40888008058517782f82e
superrrk <89561578+superrrk@users.noreply.github.com>
2024-01-11 09:34:58 -0500

    day 3
    day 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import LinkedIn from "./pictures/LinkedIn.jpeg"
+import NavigationBar from './NavigationBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='Main'>
         <h1 id = "name">
           kelly truong
         </h1>
@@ -16,29 +18,18 @@ function App() {
           im building up a little website about my little journey in JADE + beyond
         </p>
 
-        <div id = "nav bar"> 
-        <ul>
-          <li>
-            <a href ="/myportfolio/main.jsx">home</a>
-            <a href ="main.jsx">portfolio</a>
-            <a href ="main.jsx">projects</a>
-            <a href ="main.jsx">resume</a>
-          </li>
-        </ul>
+        <NavigationBar></NavigationBar>
+
+        <div id = "socials">
+          <a href="https://www.linkedin.com/in/kellybtruong/" target="_blank">
+            <img src={LinkedIn} id="LinkedIn" />
+          </a>
         </div>
 
-        <div className="card">
+        <div className="test">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-
-        <a href="https://www.linkedin.com/in/kellybtruong/" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-
         </div>
 
 
---
+++ src/NavigationBar.jsx
@@ -0,0 +1,21 @@
+import './NavigationBar.css'
+
+function NavigationBar(){
+    return(
+           <>
+           <nav>
+            <div id = "navbar"> 
+                <ul>
+                 <li><a href ="#/main">home</a></li>
+                <li><a href ="#/portfolio">portfolio</a></li>
+                <li><a href ="#/projects">projects</a></li>
+                <li><a href ="#/esumes">resume</a></li>
+                </ul>
+            </div>
+           </nav>
+            
+           </>
+    )
+}
+
+export default NavigationBar
